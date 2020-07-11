834bab4f74345c556456d0003e18db0f7c307662
giorgiocost <gegiorgio1991@gmail.com>
2020-07-10 23:10:03 -0300

    component observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservablesComponent
   ],
   imports: [
     BrowserModule,
