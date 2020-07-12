8dd193e53b9344d3d4a467703e38f778255d3d32
giorgiocost <gegiorgio1991@gmail.com>
2020-07-11 23:35:30 -0300

    trabalhando com promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projeto';
+
+  ngOnInit(): void {
+    // this.minhaPromise('Giorgio')
+    // .then(result => {
+    //   console.log(result);
+    // });
+
+    this.minhaPromise('Pedro')
+    .then(result => console.log(result))
+    .catch(err => console.error(err));
+  }
+
+  minhaPromise(nome: string ): Promise<string> {
+    return  new Promise((resolve, reject) => {
+      if( nome === 'Giorgio'){
+        setTimeout(() => {
+          resolve('seja bem vindo ' + nome);
+        },1000);
+      }else {
+        reject('ops ! você não é o Giorgio');
+      }
+    })
+  }
+
+
 }
